2936ed07691b3ef983d41639db4dc7778a4159fc
kranthi kumar <2019271@iiitdmj.ac.in>
2023-08-19 15:09:46 +0530

    updated

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,132 +1,162 @@
-"use client"
+'use client';
 
-import * as z from "zod"
-import axios from "axios"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { toast } from "react-hot-toast"
-import { Trash } from "lucide-react"
-import { Store } from "@prisma/client"
-import { useParams, useRouter } from "next/navigation"
-import { useState } from "react"
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Trash } from 'lucide-react';
+import { Store } from '@prisma/client';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Separator } from "@/components/ui/separator"
-import { Heading } from "@/components/ui/heading"
-import { AlertModal } from "@/components/modals/alert-modal"
-import { ApiAlert } from "@/components/ui/api-alert"
-import { useOrigin } from "@/hooks/use-origin"
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Separator } from '@/components/ui/separator';
+import { Heading } from '@/components/ui/heading';
+import { AlertModal } from '@/components/modals/alert-modal';
+import { ApiAlert } from '@/components/ui/api-alert';
+import { useOrigin } from '@/hooks/use-origin';
 
+// Define the validation schema using zod
 const formSchema = z.object({
-  name: z.string().min(2),
+	name: z.string().min(2), // Name field validation
 });
 
-type SettingsFormValues = z.infer<typeof formSchema>
+// Infer the type of form values from the validation schema
+type SettingsFormValues = z.infer<typeof formSchema>;
 
+// Define props interface for SettingsForm component
 interface SettingsFormProps {
-  initialData: Store;
-};
+	initialData: Store; // Initial store data
+}
 
-export const SettingsForm: React.FC<SettingsFormProps> = ({
-  initialData
-}) => {
-  const params = useParams();
-  const router = useRouter();
-  const origin = useOrigin();
+// SettingsForm component that displays and manages store settings
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+	// Get route parameters and router object using custom hooks
+	const params = useParams();
+	const router = useRouter();
 
-  const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
+	// Get API origin using custom hook
+	const origin = useOrigin();
 
-  const form = useForm<SettingsFormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues: initialData
-  });
+	// Define local state variables
+	const [open, setOpen] = useState(false); // For displaying alert modal
+	const [loading, setLoading] = useState(false); // For managing loading state
 
-  const onSubmit = async (data: SettingsFormValues) => {
-    try {
-      setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
-      router.refresh();
-      toast.success('Store updated.');
-    } catch (error: any) {
-      toast.error('Something went wrong.');
-    } finally {
-      setLoading(false);
-    }
-  };
+	// Initialize react-hook-form with validation schema and default values
+	const form = useForm<SettingsFormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: initialData,
+	});
 
-  const onDelete = async () => {
-    try {
-      setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
-      router.refresh();
-      router.push('/');
-      toast.success('Store deleted.');
-    } catch (error: any) {
-      toast.error('Make sure you removed all products and categories first.');
-    } finally {
-      setLoading(false);
-      setOpen(false);
-    }
-  }
+	// Function to handle form submission
+	const onSubmit = async (data: SettingsFormValues) => {
+		try {
+			setLoading(true); // Set loading state
+			await axios.patch(`/api/stores/${params.storeId}`, data); // Send patch request to update store data
+			router.refresh(); // Refresh the current route
+			toast.success('Store updated.'); // Display success toast
+		} catch (error: any) {
+			toast.error('Something went wrong.'); // Display error toast
+		} finally {
+			setLoading(false); // Reset loading state
+		}
+	};
 
-  return (
-    <>
-    <AlertModal 
-      isOpen={open} 
-      onClose={() => setOpen(false)}
-      onConfirm={onDelete}
-      loading={loading}
-    />
-     <div className="flex items-center justify-between">
-        <Heading title="Store settings" description="Manage store preferences" />
-        <Button
-          disabled={loading}
-          variant="destructive"
-          size="sm"
-          onClick={() => setOpen(true)}
-        >
-          <Trash className="h-4 w-4" />
-        </Button>
-      </div>
-      <Separator />
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-          <div className="grid grid-cols-3 gap-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input disabled={loading} placeholder="Store name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <Button disabled={loading} className="ml-auto" type="submit">
-            Save changes
-          </Button>
-        </form>
-      </Form>
-      <Separator />
-      <ApiAlert 
-        title="NEXT_PUBLIC_API_URL" 
-        variant="public" 
-        description={`${origin}/api/${params.storeId}`}
-      />
-    </>
-  );
+	// Function to handle store deletion
+	const onDelete = async () => {
+		try {
+			setLoading(true); // Set loading state
+			await axios.delete(`/api/stores/${params.storeId}`); // Send delete request to delete store
+			router.refresh(); // Refresh the current route
+			router.push('/'); // Navigate to the home page
+			toast.success('Store deleted.'); // Display success toast
+		} catch (error: any) {
+			toast.error('Make sure you removed all products and categories first.'); // Display error toast
+		} finally {
+			setLoading(false); // Reset loading state
+			setOpen(false); // Close the modal
+		}
+	};
+
+	// Render the SettingsForm component
+	return (
+		<>
+			{/* Display AlertModal for store deletion */}
+			<AlertModal
+				isOpen={open}
+				onClose={() => setOpen(false)}
+				onConfirm={onDelete}
+				loading={loading}
+			/>
+			{/* Heading and delete button */}
+			<div className='flex items-center justify-between'>
+				<Heading
+					title='Store settings'
+					description='Manage store preferences'
+				/>
+				<Button
+					disabled={loading}
+					variant='destructive'
+					size='sm'
+					onClick={() => setOpen(true)}
+				>
+					<Trash className='h-4 w-4' />
+				</Button>
+			</div>
+			<Separator />
+			{/* Form for updating store settings */}
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='space-y-8 w-full'
+				>
+					{/* Form fields */}
+					<div className='grid grid-cols-3 gap-8'>
+						<FormField
+							control={form.control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input
+											disabled={loading}
+											placeholder='Store name'
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					{/* Submit button */}
+					<Button
+						disabled={loading}
+						className='ml-auto'
+						type='submit'
+					>
+						Save changes
+					</Button>
+				</form>
+			</Form>
+			<Separator />
+			{/* Display API origin */}
+			<ApiAlert
+				title='NEXT_PUBLIC_API_URL'
+				variant='public'
+				description={`${origin}/api/${params.storeId}`}
+			/>
+		</>
+	);
 };
--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,74 +1,81 @@
-import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs";
+import { NextResponse } from 'next/server'; // Import NextResponse from next/server
+import { auth } from '@clerk/nextjs'; // Import authentication function from Clerk
 
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb'; // Import the prismadb instance
 
-
+// PATCH endpoint to update store data
 export async function PATCH(
-  req: Request,
-  { params }: { params: { storeId: string } }
+	req: Request,
+	{ params }: { params: { storeId: string } }
 ) {
-  try {
-    const { userId } = auth();
-    const body = await req.json();
+	try {
+		const { userId } = auth(); // Get the authenticated user's ID using Clerk's auth()
 
-    const { name } = body;
+		const body = await req.json(); // Parse the request body
+		const { name } = body; // Extract the 'name' field from the body
 
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 403 });
-    }
+		// Check if the user is not authenticated
+		if (!userId) {
+			return new NextResponse('Unauthenticated', { status: 403 }); // Return forbidden status
+		}
 
-    if (!name) {
-      return new NextResponse("Name is required", { status: 400 });
-    }
+		// Check if the 'name' field is missing
+		if (!name) {
+			return new NextResponse('Name is required', { status: 400 }); // Return bad request status
+		}
 
-    if (!params.storeId) {
-      return new NextResponse("Store id is required", { status: 400 });
-    }
+		// Check if the 'storeId' parameter is missing
+		if (!params.storeId) {
+			return new NextResponse('Store id is required', { status: 400 }); // Return bad request status
+		}
 
-    const store = await prismadb.store.updateMany({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-      data: {
-        name
-      }
-    });
-  
-    return NextResponse.json(store);
-  } catch (error) {
-    console.log('[STORE_PATCH]', error);
-    return new NextResponse("Internal error", { status: 500 });
-  }
-};
+		// Update the store using prismadb
+		const store = await prismadb.store.updateMany({
+			where: {
+				id: params.storeId,
+				userId,
+			},
+			data: {
+				name,
+			},
+		});
 
+		return NextResponse.json(store); // Return the updated store data as JSON
+	} catch (error) {
+		console.log('[STORE_PATCH]', error); // Log the error
+		return new NextResponse('Internal error', { status: 500 }); // Return internal server error status
+	}
+}
 
+// DELETE endpoint to delete a store
 export async function DELETE(
-  req: Request,
-  { params }: { params: { storeId: string } }
+	req: Request,
+	{ params }: { params: { storeId: string } }
 ) {
-  try {
-    const { userId } = auth();
+	try {
+		const { userId } = auth(); // Get the authenticated user's ID using Clerk's auth()
 
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 403 });
-    }
+		// Check if the user is not authenticated
+		if (!userId) {
+			return new NextResponse('Unauthenticated', { status: 403 }); // Return forbidden status
+		}
 
-    if (!params.storeId) {
-      return new NextResponse("Store id is required", { status: 400 });
-    }
+		// Check if the 'storeId' parameter is missing
+		if (!params.storeId) {
+			return new NextResponse('Store id is required', { status: 400 }); // Return bad request status
+		}
 
-    const store = await prismadb.store.deleteMany({
-      where: {
-        id: params.storeId,
-        userId
-      }
-    });
-  
-    return NextResponse.json(store);
-  } catch (error) {
-    console.log('[STORE_DELETE]', error);
-    return new NextResponse("Internal error", { status: 500 });
-  }
-};
+		// Delete the store using prismadb
+		const store = await prismadb.store.deleteMany({
+			where: {
+				id: params.storeId,
+				userId,
+			},
+		});
+
+		return NextResponse.json(store); // Return the deleted store data as JSON
+	} catch (error) {
+		console.log('[STORE_DELETE]', error); // Log the error
+		return new NextResponse('Internal error', { status: 500 }); // Return internal server error status
+	}
+}
